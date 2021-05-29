9f9534965a8f26ff65a9f01fbaafa8755d0fe849
Khoa Nguyen <khoang323@hotmail.com>
2021-05-29 17:32:34 -0600

    refactor view

--- app/poros/doge_coin.rb
+++ app/poros/doge_coin.rb
@@ -9,7 +9,10 @@ class DogeCoin
     past_total = bought_price * quantity
     current_total = current_price * quantity
     profit_and_lost = current_total - past_total
-    profit_and_lost
+    {
+      profit_and_lost: profit_and_lost,
+      total_cost: bought_price * quantity,
+      total: current_price * quantity
+    }
   end
-  
 end
